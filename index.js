6169be3170f1eac2a151a05002c1ec05e11025c8
vinayshetty777 <70328111+vinayshetty777@users.noreply.github.com>
2020-09-10 20:51:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+
+  var num=Math.random();
+  var num1= 7*num;
+  var realnum=Math.floor(num1);
+  if(realnum==1)
+  {
+      document.querySelectorAll("img")[0].setAttribute("src","images/dice1.png");
+
+   }
+  else if(realnum==2)
+   {
+      document.querySelectorAll("img")[0].setAttribute("src","images/dice2.png");
+
+   }
+   else if(realnum==3)
+   {
+      document.querySelectorAll("img")[0].setAttribute("src","images/dice3.png")
+
+   }
+   else if(realnum==4)
+   {
+      document.querySelectorAll("img")[0].setAttribute("src","images/dice4.png");
+
+   }
+   else if(realnum==5)
+   {
+      document.querySelectorAll("img")[0].setAttribute("src","images/dice5.png");
+
+   }
+   else
+   {
+       document.querySelectorAll("img")[0].setAttribute("src","images/dice6.png");
+
+   }
+   var num=Math.random();
+   var num1= 7*num;
+   var realnum=Math.floor(num1);
+   if(realnum==1)
+   {
+       document.querySelectorAll("img")[1].setAttribute("src","images/dice1.png");
+       
+    }
+   else if(realnum==2)
+    {
+       document.querySelectorAll("img")[1].setAttribute("src","images/dice2.png");
+
+    }
+    else if(realnum==3)
+    {
+       document.querySelectorAll("img")[1].setAttribute("src","images/dice3.png")
+
+    }
+    else if(realnum==4)
+    {
+       document.querySelectorAll("img")[1].setAttribute("src","images/dice4.png");
+
+    }
+    else if(realnum==5)
+    {
+       document.querySelectorAll("img")[1].setAttribute("src","images/dice5.png");
+
+    }
+    else
+    {
+        document.querySelectorAll("img")[1].setAttribute("src","images/dice6.png");
+
+    }
