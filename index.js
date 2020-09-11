9c20cf54a4cbf12be8702af14de635ae9b514006
vinayshetty777 <70328111+vinayshetty777@users.noreply.github.com>
2020-09-11 15:43:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-
+ document.querySelectorAll("img")[0].addEventListener("click",callFun1);
+function callFun1()
+{
   var num=Math.random();
   var num1= 7*num;
   var realnum=Math.floor(num1);
@@ -32,36 +34,41 @@
        document.querySelectorAll("img")[0].setAttribute("src","images/dice6.png");
 
    }
-   var num=Math.random();
-   var num1= 7*num;
-   var realnum=Math.floor(num1);
-   if(realnum==1)
-   {
-       document.querySelectorAll("img")[1].setAttribute("src","images/dice1.png");
-       
-    }
-   else if(realnum==2)
+ }
+ document.querySelectorAll("img")[1].addEventListener("click",callFun2);
+ function callFun2()
+ {
+    var num=Math.random();
+    var num1= 7*num;
+    var realnum=Math.floor(num1);
+    if(realnum==1)
     {
-       document.querySelectorAll("img")[1].setAttribute("src","images/dice2.png");
+        document.querySelectorAll("img")[1].setAttribute("src","images/dice1.png");
 
-    }
-    else if(realnum==3)
-    {
-       document.querySelectorAll("img")[1].setAttribute("src","images/dice3.png")
+     }
+    else if(realnum==2)
+     {
+        document.querySelectorAll("img")[1].setAttribute("src","images/dice2.png");
 
-    }
-    else if(realnum==4)
-    {
-       document.querySelectorAll("img")[1].setAttribute("src","images/dice4.png");
+     }
+     else if(realnum==3)
+     {
+        document.querySelectorAll("img")[1].setAttribute("src","images/dice3.png")
 
-    }
-    else if(realnum==5)
-    {
-       document.querySelectorAll("img")[1].setAttribute("src","images/dice5.png");
+     }
+     else if(realnum==4)
+     {
+        document.querySelectorAll("img")[1].setAttribute("src","images/dice4.png");
 
-    }
-    else
-    {
-        document.querySelectorAll("img")[1].setAttribute("src","images/dice6.png");
+     }
+     else if(realnum==5)
+     {
+        document.querySelectorAll("img")[1].setAttribute("src","images/dice5.png");
 
-    }
+     }
+     else
+     {
+         document.querySelectorAll("img")[1].setAttribute("src","images/dice6.png");
+
+     }
+ }
